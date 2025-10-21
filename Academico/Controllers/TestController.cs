974a292a16896cbc8fd21e036261771a23d4a948
laurasferreiras <laurasferreira6@gmail.com>
2025-10-21 19:52:58 -0300

    Action Aula

---
+++ Academico/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Academico.Controllers
+{
+    public class TestController : Controller
+    {
+        private readonly ILogger<TestController> _logger;
+
+        public TestController(ILogger<TestController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Aula()
+        {
+            return View();
+        }
+
+    }
+}
