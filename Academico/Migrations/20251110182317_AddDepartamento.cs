bd75774b5c3b11c5978563caa790a964b2d87213
laurasferreiras <laurasferreira6@gmail.com>
2025-11-10 15:39:14 -0300

    Tentativa classe Departamento

---
+++ Academico/Migrations/20251110182317_AddDepartamento.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Academico.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDepartamento : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Departamentos",
+                columns: table => new
+                {
+                    DepartamentoID = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Departamentos", x => x.DepartamentoID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Departamentos");
+        }
+    }
+}
---
+++ Academico/Models/Departamento.cs
@@ -0,0 +1,10 @@
+﻿namespace Academico.Models
+{
+    public class Departamento
+    {
+            public long DepartamentoID { get; set; }
+            public string Nome { get; set; }
+    }
+}
+
+
